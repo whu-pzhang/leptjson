4db9ad32b4d66b6f8617b29b5aa80f372ff2f89d
whu-pzhang <pzhang@whu.edu.cn>
2018-06-13 12:30:03 +0800

    重构了解析字面值的函数（lept_parse_true/false/null），减少重复代码

--- leptjson.c
+++ leptjson.c
@@ -13,9 +13,11 @@ typedef struct {
     const char *json;
 } lept_context;
 
-// static void lept_parse_whitespace(lept_context *c);
-// static int lept_parse_null(lept_context *c, lept_value *v);
-// static int lept_parse_value(lept_context *c, lept_value *v);
+/*
+static void lept_parse_whitespace(lept_context *c);
+static int lept_parse_null(lept_context *c, lept_value *v);
+static int lept_parse_value(lept_context *c, lept_value *v);
+*/
 
 /* ws = *(%x20 / %x09 / % x0A / %x0D) */
 static void lept_parse_whitespace(lept_context *c)
@@ -26,37 +28,17 @@ static void lept_parse_whitespace(lept_context *c)
     c->json = p;
 }
 
-/* null = "null" */
-static int lept_parse_null(lept_context *c, lept_value *v)
+/* refactoring lept_parse_true/false/null */
+static int lept_parse_literal(lept_context *c, lept_value *v,
+                              const char *literal, lept_type type)
 {
-    EXPECT(c, 'n');
-    if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json += 3;
-    v->type = LEPT_NULL;
-    return LEPT_PARSE_OK;
-}
-
-/* false = "false" */
-static int lept_parse_false(lept_context *c, lept_value *v)
-{
-    EXPECT(c, 'f');
-    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' ||
-        c->json[3] != 'e')
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json += 4;
-    v->type = LEPT_FALSE;
-    return LEPT_PARSE_OK;
-}
-
-/* false = "false" */
-static int lept_parse_true(lept_context *c, lept_value *v)
-{
-    EXPECT(c, 't');
-    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json += 3;
-    v->type = LEPT_TRUE;
+    size_t i;
+    EXPECT(c, literal[0]);
+    for (i = 0; literal[i + 1]; i++)
+        if (c->json[i] != literal[i + 1])
+            return LEPT_PARSE_INVALID_VALUE;
+    c->json += i;
+    v->type = type;
     return LEPT_PARSE_OK;
 }
 
@@ -65,11 +47,11 @@ static int lept_parse_value(lept_context *c, lept_value *v)
 {
     switch (*c->json) {
     case 'n':
-        return lept_parse_null(c, v);
+        return lept_parse_literal(c, v, "null", LEPT_NULL);
     case 'f':
-        return lept_parse_false(c, v);
+        return lept_parse_literal(c, v, "false", LEPT_FALSE);
     case 't':
-        return lept_parse_true(c, v);
+        return lept_parse_literal(c, v, "true", LEPT_TRUE);
     case '\0':
         return LEPT_PARSE_EXPECT_VALUE;
     default:
